698352f1c01c95c1ca297e47cc8822ae8085b0ae
amatranga <amatranga892@gmail.com>
2017-10-17 18:38:23 -0700

    Implement jsx files
    index.jsx
    
    WhatFourChords.jsx is the parent rendered by index.jsx

---
+++ client/src/components/Chords.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chords = props => {
+  return (
+    <div className="col-sm-12 text-center">
+      Chords Component Goes Here
+    </div>
+  );
+}
+
+export default Chords;
